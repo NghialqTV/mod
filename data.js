520193f467cd7607467eeb60a87e08674f6ae46e
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-01-08 00:29:13 +0700

    Add games, keys, and files to data.js

--- data.js
+++ data.js
@@ -3,6 +3,50 @@ const SOCIAL={
  youtube:"https://youtube.com/@yourchannel"
 };
 
-const APPS=[
- {name:"Pack 54 Skin LQ",version:"11/7",logo:"assets/img/app.png",link:"https://yourdownloadlink.com"}
+/* ===== TẢI GAME ===== */
+const GAMES = [
+ {
+  name: "Hack CF Mobile IOS No Key",
+  update: "Cập nhật 2/1",
+  icon: "assets/img/cf.png",
+  link: "https://link-cf.com"
+ },
+ {
+  name: "Hack Map AOV 6.0",
+  update: "Cập nhật 03/01",
+  icon: "assets/img/aov.png",
+  link: "https://link-aov.com"
+ },
+ {
+  name: "Unlock Skin LQ Android",
+  update: "Cập nhật 19/12",
+  icon: "assets/img/lq-android.png",
+  link: "https://link-lq.com"
+ }
+];
+
+/* ===== GET KEY ===== */
+const KEYS = [
+ {
+  name: "Get Key LQ IOS (Mod Skin)",
+  update: "Vượt 1 link – 1.5 Day",
+  icon: "assets/img/key.png",
+  link: "https://link-key-ios.com"
+ },
+ {
+  name: "Get Key LQ Android",
+  update: "Vượt 1 link – 1 Day",
+  icon: "assets/img/key.png",
+  link: "https://link-key-android.com"
+ }
+];
+
+/* ===== FILE MOD ===== */
+const FILES = [
+ {
+  name: "Hướng Dẫn Mod Dán File IOS",
+  update: "Cập nhật 2/11",
+  icon: "assets/img/file.png",
+  link: "https://link-file-ios.com"
+ }
 ];
