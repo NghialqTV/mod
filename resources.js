208c252ccae6ad18780069002083be0b03008235
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-01-13 21:25:41 +0700

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,21 @@
+fetch("data/resources.json?v=" + Date.now())
+  .then(res => res.json())
+  .then(list => {
+    document.getElementById("resources").innerHTML =
+      list.map(i => `
+        <div class="card">
+          <img class="icon" src="${i.icon}">
+          <div class="info">
+            <b>${i.name}</b>
+            <div>${i.version || ""}</div>
+          </div>
+          <a href="${i.link}" target="_blank">✓</a>
+        </div>
+
+        ${i.banner ? `
+          <div class="file-banner">
+            <img src="${i.banner}">
+          </div>
+        ` : ``}
+      `).join("");
+  });
