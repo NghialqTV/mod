284ba07c9aa33d58e0ba9ca0954e3ccc3c9d9eda
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-05-18 19:13:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,54 +7,81 @@ function openWithAd(url){
 
   window.adLoading = true;
 
-  // mở quảng cáo
-  window.open(
+  // mở quảng cáo trực tiếp
+  const ad = window.open(
     "https://omg10.com/4/11023287",
     "_blank"
   );
 
-  // mở link sau quảng cáo
-  setTimeout(()=>{
-    window.open(url, "_blank");
-  },1500);
+  // nếu bị chặn popup thì mở cùng tab
+  if(!ad || ad.closed || typeof ad.closed == "undefined"){
+    window.location.href = "https://omg10.com/4/11023287";
+    return;
+  }
+
+  // mở link chính sau ads
+  setTimeout(() => {
+    window.location.href = url;
+  }, 1200);
 
   // reset
-  setTimeout(()=>{
+  setTimeout(() => {
     window.adLoading = false;
-  },3000);
+  }, 3000);
 }
 
 /* ===== RENDER DATA ===== */
 
 function render(url, boxId){
-  fetch(url + "?v=" + Date.now(), { cache: "no-store" })
-    .then(res => {
-      if(!res.ok) throw new Error("Fetch lỗi " + url);
-      return res.json();
-    })
-    .then(data => {
-      document.getElementById(boxId).innerHTML =
-        data.map(i => `
-          <div class="card">
-            <img class="icon" src="${i.icon}">
-            <div class="info">
-              <b>${i.name}</b>
-              <div>${i.version || ""}</div>
-            </div>
 
-            <a href="#"
-               onclick="openWithAd('${i.link}'); return false;">
-               ✓
-            </a>
+  fetch(url + "?v=" + Date.now(), {
+    cache: "no-store"
+  })
 
+  .then(res => {
+
+    if(!res.ok)
+      throw new Error("Fetch lỗi " + url);
+
+    return res.json();
+
+  })
+
+  .then(data => {
+
+    document.getElementById(boxId).innerHTML =
+
+      data.map(i => `
+
+        <div class="card">
+
+          <img class="icon" src="${i.icon}">
+
+          <div class="info">
+            <b>${i.name}</b>
+            <div>${i.version || ""}</div>
           </div>
-        `).join("");
-    })
-    .catch(err => {
-      document.getElementById(boxId).innerHTML =
-        `<div style="color:red">Lỗi load dữ liệu</div>`;
-      console.error(err);
-    });
+
+          <a href="#"
+             onclick="openWithAd('${i.link}'); return false;">
+             ✓
+          </a>
+
+        </div>
+
+      `).join("");
+
+  })
+
+  .catch(err => {
+
+    document.getElementById(boxId).innerHTML =
+      `<div style="color:red">Lỗi load dữ liệu</div>`;
+
+    console.error(err);
+
+  });
+
 }
 
 render("data/apps.json", "apps");
@@ -63,13 +90,21 @@ render("data/keys.json", "keys");
 /* ===== FILES ===== */
 
 function renderFiles(){
+
   fetch("data/files.json")
+
     .then(res => res.json())
+
     .then(files => {
+
       document.getElementById("files").innerHTML =
+
         files.map(f => `
+
           <div class="card">
+
             <img class="icon" src="${f.icon}">
+
             <div class="info">
               <b>${f.name}</b>
               <div>${f.version}</div>
@@ -83,12 +118,17 @@ function renderFiles(){
           </div>
 
           ${f.banner ? `
+
             <div class="file-banner">
               <img src="${f.banner}">
             </div>
+
           ` : ``}
+
         `).join("");
+
     });
+
 }
 
 renderFiles();
@@ -125,6 +165,7 @@ function createMai(){
   maiFall.appendChild(m);
 
   setTimeout(() => m.remove(), 10000);
+
 }
 
 setInterval(createMai, 500);
@@ -132,10 +173,13 @@ setInterval(createMai, 500);
 /* ===== MOD SKIN ===== */
 
 fetch("data/mods.json")
+
   .then(res => res.json())
+
   .then(mods => {
 
     document.getElementById("mods").innerHTML =
+
       mods.map(m => `
 
         <a href="#"
@@ -149,6 +193,7 @@ fetch("data/mods.json")
         </a>
 
       `).join("");
+
   });
 
 /* ===== VOICE + MUSIC ===== */
@@ -169,6 +214,7 @@ function speakWelcome(){
   msg.pitch = 1.3;
 
   speechSynthesis.speak(msg);
+
 }
 
 enterBtn.addEventListener("click", () => {
@@ -177,27 +223,4 @@ enterBtn.addEventListener("click", () => {
 
   speakWelcome();
 
-  bgMusic.volume = 0.4;
-
-  bgMusic.play().catch(()=>{});
-
-});
-
-/* ===== DARK MODE TOGGLE ===== */
-
-const toggle = document.getElementById("darkToggle");
-
-if(toggle){
-
-  toggle.onclick = () => {
-
-    document.body.classList.toggle("dark");
-
-    localStorage.setItem(
-      "dark",
-      document.body.classList.contains("dark")
-    );
-
-  };
-
-}
+  bgMusic
