3606d344a105eca94fccadbf4628bbc7a0898d29
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-08-26 17:45:26 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11655143
+    "zoneId": 11656096
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
