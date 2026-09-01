109f62d6e36a28067e177bb1a14cfa41c57f75d6
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-09-02 00:14:02 +0700

    Update TikTok ad links in config.js

--- tiktok_ads/config.js
+++ tiktok_ads/config.js
@@ -2,6 +2,6 @@ window.TIKTOK_AD_LINKS = [
   "https://vt.tiktok.com/ZS9Bx4RQG7sTR-1teGe/",
   "https://vt.tiktok.com/ZS9BxXw9wUR7C-QZnPz/",
   "https://vt.tiktok.com/ZS9BfYXq4SUVb-HB6li/",
-  "https://vt.tiktok.com/ZS9BxXv697Nfw-rNVQC/",
-  "https://vt.tiktok.com/ZS9BxXVEK1y2g-lgDec/"
+  "https://vt.tiktok.com/ZS9Bsh29NRboX-rzCEr/",
+  "https://vt.tiktok.com/ZS9Bsh8v7htQC-0dVzk/"
 ];
